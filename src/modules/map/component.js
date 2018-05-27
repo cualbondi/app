740252aa96a61a8cd80c00cd338e32f5f5fe0e0a
Bruno Cascio <brunocascio@gmail.com>
2018-05-27 17:23:32 -0300

    Overlay hotfixes

--- src/modules/map/component.js
+++ src/modules/map/component.js
@@ -51,50 +51,53 @@ export default class MapScene extends React.Component {
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
+        />
+        <View
+          pointerEvents='box-none'
+          style={styles.overlayContainer}
         >
-          <View style={{ flex: 1 }}>
-            <View style={styles.inputContainer}>
-              <InputItem
-                onFocus={this.onFocusInput}
-                onBlur={this.onBlurInput}
-                placeholder={'Desde'}
-                color={'#fff'}
-                fontWeight={'600'}
-                placeholderTextColor={'#e5e5e5'}
-                style={{ 
-                  backgroundColor: '#6099F3'
-                }}
-              />
-              <InputItem 
-                placeholder={'Hasta'}
-                color={'#fff'}
-                fontWeight={'600'}
-                placeholderTextColor={'#e5e5e5'}
-                style={{
-                  marginTop: 10,
-                  backgroundColor: '#6099F3'
-                }}
-              />
-            </View>
-            {this.state.focusedInput !== null && (
-              <Animatable.View 
-                animation={this.state.focusedInput ? 'bounceInRight' : 'bounceOutRight'} 
-                style={styles.inputFocusedContainer}
-              >
-                <List renderHeader={() => 'Favoritos'}>
-                  <List.Item onClick={e => alert('hola')} arrow={'horizontal'}>
-                    <Text>Mi Ubicación</Text>
-                  </List.Item>
-                </List>
-                <List renderHeader={() => 'Resultados de Búsqueda'}>
-                  <List.Item>
-                    <Text>Calle 12 #1503</Text>
-                  </List.Item>
-                </List>
-              </Animatable.View>
-            )}
+          <View style={styles.inputContainer}>
+            <InputItem
+              onFocus={this.onFocusInput}
+              onBlur={this.onBlurInput}
+              placeholder={'Desde'}
+              color={'#fff'}
+              fontWeight={'600'}
+              placeholderTextColor={'#e5e5e5'}
+              style={{
+                backgroundColor: '#6099F3'
+              }}
+            />
+            <InputItem
+              onFocus={this.onFocusInput}
+              onBlur={this.onBlurInput}
+              placeholder={'Hasta'}
+              color={'#fff'}
+              fontWeight={'600'}
+              placeholderTextColor={'#e5e5e5'}
+              style={{
+                marginTop: 10,
+                backgroundColor: '#6099F3'
+              }}
+            />
           </View>
-        </MapView>
+          {this.state.focusedInput && (
+            <View
+              style={styles.resultsContainer}
+            >
+              <List renderHeader={() => 'Favoritos'}>
+                <List.Item onClick={e => alert('hola')} arrow={'horizontal'}>
+                  <Text>Mi Ubicación</Text>
+                </List.Item>
+              </List>
+              <List renderHeader={() => 'Resultados de Búsqueda'}>
+                <List.Item>
+                  <Text>Calle 12 #1503</Text>
+                </List.Item>
+              </List>
+            </View>
+          )}
+        </View>
       </View>
     );
   }
@@ -108,14 +111,19 @@ const styles = StyleSheet.create({
   mapContainer: {
     flex: 1
   },
+  overlayContainer: {
+    ...StyleSheet.absoluteFillObject
+  },
   inputContainer: {
+    // flex: 1,
+    // maxHeight: 150,
     backgroundColor: '#4688F1', 
     paddingTop: 30, 
     paddingBottom: 10, 
     paddingHorizontal: 15
   },
-  inputFocusedContainer: {
-    flex: 6,
-    backgroundColor: '#E9EBEE'
+  resultsContainer: {
+    flex: 1,
+    backgroundColor: '#fff'
   }
 });
