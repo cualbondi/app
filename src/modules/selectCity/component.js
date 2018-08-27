53f670918d9b7377777050237d0322f996ae2285
Bruno Cascio <brunocascio@gmail.com>
2018-08-26 22:24:46 -0300

    Hotfix hmr

--- src/modules/selectCity/component.js
+++ src/modules/selectCity/component.js
@@ -1,16 +1,49 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StatusBar, Text, StyleSheet, Image } from 'react-native'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-export default (props) => (
-  <View style={styles.container}>
-    <Text>Hola</Text>
-  </View>
-)
+class SelectCity extends React.PureComponent {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View>
+          <Image 
+            source={require('@assets/images/example.png')}
+          />
+        </View>
+        <View style={styles.list}>
+          {this.props.cities.map((c, i) => 
+            <Text key={i} style={styles.listItem}>{c.nombre}</Text>
+          )}
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  list: {
+    backgroundColor: '#42BFE5',
     flex: 1,
-    backgroundColor: 'blue',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  listItem: {
+    flex: 1,
+    justifyContent: 'space-between',
+    color: 'white',
+    fontSize: 20
   }
 })
+
+export default connect(
+  (state) => ({
+    cities: state.cities.list
+  }),
+  (dispatch) => bindActionCreators({}, dispatch)
+)(SelectCity)
